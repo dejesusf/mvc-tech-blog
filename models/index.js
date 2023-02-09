e263a313170cc706324115857ac2ce0ec176e2b7
dejesusf <dejesusf@uw.edu>
2023-02-09 09:38:10 -0800

    added new route files

--- models/index.js
+++ models/index.js
@@ -1,12 +1,19 @@
 const User = require("./user");
 const Post = require("./post");
+const Comment= require("./comment")
 
+//a post belongs to a user
 Post.belongsTo(User,{
     onDelete:"CASCADE"
 })
+//a user may have many posts
 User.hasMany(Post)
+
+//a post may have many comments
+Post.hasMany(Comment)
 
 module.exports = {
     User,
-    Post
+    Post,
+    Comment
 }
